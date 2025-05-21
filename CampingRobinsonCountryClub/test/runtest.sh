11d0c7b9f00423e48a38fb76c55c2c40f9bba21e
Tibor Matei <tibor.matei@thermofisher.com>
2025-05-21 21:22:29 +0300

    Updated yml file

--- CampingRobinsonCountryClub/test/runtest.sh
+++ CampingRobinsonCountryClub/test/runtest.sh
@@ -4,4 +4,4 @@ set -e  # Exit immediately if any command fails
 cd "$(dirname "$0")"/..
 
 # Run all tests
-python -m unittest test/test_TentModel.py
+PYTHONPATH=. python -m unittest test/test_TentModel.py
