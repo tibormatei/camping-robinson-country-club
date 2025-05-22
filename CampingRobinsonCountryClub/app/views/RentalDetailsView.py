b39ad635ad83a27afc3f7765d2e0aa319c378730
Tibor Matei <tibor.matei@thermofisher.com>
2025-05-22 18:21:04 +0300

    Added View and Controller classes

---
+++ CampingRobinsonCountryClub/app/views/RentalDetailsView.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2025 Matei Tibor. All rights reserved.
+#
+# Filename: RentalDetailsView.py
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License.
+
+"""
+@summary: This class handles the view of rental details.
+"""
+
+
+class RentalDetailsView() :
+    """
+    @summary: This class handles the view of rental details.
+    """
+
+    pass #To do
